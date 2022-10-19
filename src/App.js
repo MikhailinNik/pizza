25037ba03354adeadf2f34c6049debe243ece9b9
Nikita <ifyoucan1337@yandex.ru>
2022-10-19 22:29:43 +0300

    Added pages(home, cart, notfound)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './scss/app.scss';
 
 import Header from './components/Header';
+<<<<<<< Updated upstream
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import Card from './components/Card';
 import React from 'react';
 // import data from './assets/data.json';
 import axios from 'axios';
+=======
+import Home from './pages/Home';
+>>>>>>> Stashed changes
 
 function App() {
 	const [items, setItems] = React.useState([]);
@@ -20,6 +24,7 @@ function App() {
 		<div className="wrapper">
 			<Header />
 			<div className="content">
+<<<<<<< Updated upstream
 				<div className="container">
 					<div className="content__top">
 						<Categories />
@@ -32,6 +37,9 @@ function App() {
 						))}
 					</div>
 				</div>
+=======
+				<Home />
+>>>>>>> Stashed changes
 			</div>
 		</div>
 	);
