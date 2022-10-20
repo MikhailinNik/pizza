662a522486e86bfafaa590071d6a0df3a44c8bd4
Nikita <ifyoucan1337@yandex.ru>
2022-10-20 13:33:01 +0300

    Added cart

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './scss/app.scss';
 
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFoundPage from './pages/NotFoundPage';
+import Cart from './pages/Cart';
 
 function App() {
 	return (
 		<div className="wrapper">
 			<Header />
-			<div className="content">
-				<NotFoundPage />
-			</div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:id" element={<NotFoundPage />} />
+				<Route path="/cart" element={<Cart />} />
+			</Routes>
 		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,16 +19,18 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="container">
-			<div className="content__top">
-				<Categories />
-				<Sort />
-			</div>
-			<h2 className="content__title">Все пиццы</h2>
-			<div className="content__items">
-				{isLoading
-					? [...new Array(6)].map((_, index) => <Loading key={index} />)
-					: items.map(obj => <Card key={obj.id} {...obj} />)}
+		<div className="content">
+			<div className="container">
+				<div className="content__top">
+					<Categories />
+					<Sort />
+				</div>
+				<h2 className="content__title">Все пиццы</h2>
+				<div className="content__items">
+					{isLoading
+						? [...new Array(6)].map((_, index) => <Loading key={index} />)
+						: items.map(obj => <Card key={obj.id} {...obj} />)}
+				</div>
 			</div>
 		</div>
 	);
